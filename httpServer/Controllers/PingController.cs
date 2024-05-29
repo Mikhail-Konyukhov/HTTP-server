278f0e31b713bff177a2accc042356c5cec14f2f
Mikhail-Konyukhov <konyukhov.mi@yandex.ru>
2024-05-29 16:47:31 +0300

    upload

---
+++ httpServer/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using GenHTTP;
+using Microsoft.AspNetCore.Mvc;
+
+namespace httpServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
---
+++ httpServer/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using GenHTTP;
+using Microsoft.AspNetCore.Mvc;
+
+namespace httpServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test([FromBody] string requestBody)
+        {
+            if (requestBody == null)
+            {
+                return BadRequest("Missing request body");
+            }
+
+            return Ok(requestBody + " (test string)");
+        }
+    }
+}
