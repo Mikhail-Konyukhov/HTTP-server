e142bf2fe61bc3f3c3328e2cdbbba6ebfc944736
Mikhail-Konyukhov <konyukhov.mi@yandex.ru>
2024-05-30 17:25:30 +0300

    Добавлена документация

--- httpServer/Controllers/PingController.cs
+++ httpServer/Controllers/PingController.cs
@@ -6,8 +6,15 @@ namespace httpServer.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    /// Пинг-понг для проверки работы веб-сервера
     public class PingController : ControllerBase
     {
+        /// <summary>
+        /// Обработка get-запроса по адресу /ping
+        /// </summary>
+        /// <returns>
+        /// string "pong"
+        /// </returns>
         [HttpGet]
         public IActionResult Ping()
         {
--- httpServer/Controllers/TestController.cs
+++ httpServer/Controllers/TestController.cs
@@ -6,14 +6,22 @@ namespace httpServer.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    /// Тестовый контроллер
     public class TestController : ControllerBase
     {
+        /// <summary>
+        /// Обработка get-запроса по адресу /test
+        /// </summary>
+        /// <param name="requestBody"></param>
+        /// <returns>
+        /// тело запроса + " (test string)"
+        /// </returns>
         [HttpGet]
         public IActionResult Test([FromBody] string requestBody)
         {
             if (requestBody == null)
             {
-                return BadRequest("Missing request body");
+                return BadRequest("Отсутствует тело запроса");
             }
 
             return Ok(requestBody + " (test string)");
